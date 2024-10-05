95b43cb5aa4b2c71088751653afbf1f8c430b16f
John Alex <hnaveen296@gmail.com>
2024-10-05 15:16:35 +0530

    message

---
+++ src/gitDemoTest/TestDemo.java
@@ -0,0 +1,11 @@
+package gitDemoTest;
+
+public class TestDemo {
+	
+	public static void main(String[] args) {
+		System.out.println("Demo start");
+		System.out.println("Demo in the middle");
+		System.out.println("Demo in the ends");
+	}
+
+}
